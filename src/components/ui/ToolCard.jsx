a8a998a60901937b67e32cbecb7a0276b60f1265
Asli Mich Jose J <aslimich22@gmail.com>
2025-12-30 10:41:21 +0530

    Delete src/components/ui/ToolCard.jsx

--- src/components/ui/ToolCard.jsx
+++
@@ -1,8 +0,0 @@
-export default function ToolCard({ icon, label }) {
-  return (
-    <div className="p-3 bg-gray-100 dark:bg-gray-700 rounded-lg cursor-pointer hover:bg-gray-200 dark:hover:bg-gray-600 flex flex-col items-center">
-      <div className="text-2xl text-blue-600 dark:text-blue-400">{icon}</div>
-      <p className="text-sm mt-1 dark:text-white">{label}</p>
-    </div>
-  );
-}
